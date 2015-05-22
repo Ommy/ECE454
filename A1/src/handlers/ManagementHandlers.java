f677e3c0c5deea2fbd2ca64a60a737c6f25142ab
Victor Lai <lai.victor.vl@gmail.com>
2015-05-22 09:06:55 -0400

    Updating project

---
+++ A1/src/handlers/ManagementHandlers.java
@@ -0,0 +1,11 @@
+import ece454750s15a1.*;
+
+import org.apache.thrift.TException;
+
+public class ManagementHandlers implements A1Management.Iface {
+
+    @Override
+    public List<String> getGroupMembers() throws org.apache.thrift.TException {
+        return null;
+    }
+}
--- A1/src/management.thrift
+++ A1/src/management.thrift
@@ -1,5 +1,12 @@
 namespace java ece454750s15a1
 
+struct PerfCounters {
+    1:i32 numSecondUp,
+    2:i32 numRequestsReceived,
+    3:i32 numRequestsCompleted
+}
+
 service A1Management {
     list<string> getGroupMembers()
+    PerfCounters getPerfCounters()
 }

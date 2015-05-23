645048ee9e80dc2f5043b2e3953893e70aaefa0a
Fasih Awan <fasihawan@gmail.com>
2015-05-23 19:22:56 -0400

    Added in Client code, and FEServer code

--- A1/src/clients/JavaClient.java
+++ A1/src/clients/JavaClient.java
@@ -11,20 +11,29 @@ import org.apache.thrift.protocol.TProtocol;
 
 public class JavaClient {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Please enter 'simple' or 'secure'");
+            System.exit(0);
+        }
+        TTransport transport = null;
+        try {
+            transport = new TSocket("localhost", 9090);
+            transport.open();
 
-    if (args.length != 1) {
-      System.out.println("Please enter 'simple' or 'secure'");
-      System.exit(0);
+            TProtocol protocol = new TBinaryProtocol(transport);
+            A1Password.Client client = new A1Password.Client(protocol);
+
+            perform(client);
+        } catch (TException x) {
+            x.printStackTrace();
+        } finally {
+            transport.close();
+        }
     }
-    TTransport transport = null;
-    try {
-      transport = new TSocket("localhost", 9090);
-      transport.open();
-    } catch (TException x) {
-      x.printStackTrace();
-    } finally {
-      transport.close();
+
+    public static void perform(A1Password.Client client) throws TException {
+        String pass = client.hashPassword("hunter2", (short)10);
+        System.out.println(pass);
     }
-  }
 }
--- A1/src/handlers/PasswordHandler.java
+++ A1/src/handlers/PasswordHandler.java
@@ -3,16 +3,19 @@ package handlers;
 import ece454750s15a1.A1Password;
 import ece454750s15a1.ServiceUnavailableException;
 import org.apache.thrift.TException;
+import org.mindrot.jbcrypt.BCrypt;
 
 public class PasswordHandler implements A1Password.Iface {
 
     @Override
     public String hashPassword(String password, short logRounds) throws ServiceUnavailableException, TException {
-        return null;
+        String hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt(logRounds));
+        return hashedPassword;
     }
 
     @Override
     public boolean checkPassword(String password, String hash) throws TException {
-        return false;
+        boolean result = BCrypt.checkpw(password, hash);
+        return result;
     }
 }

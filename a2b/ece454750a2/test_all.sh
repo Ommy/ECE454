7b2cff9bbabc431008f6a6cfba0b3ae7094dcd79
Fasih Awan <fasihawan@gmail.com>
2015-06-28 23:19:41 -0400

    Added check for input into script

--- a2b/ece454750a2/test_all.sh
+++ a2b/ece454750a2/test_all.sh
@@ -1,10 +1,15 @@
-ncores=$1
+ncores=1
+
+if [ $# -eq 1 ]
+then
+    ncores=$1
+fi
 
 # Input files
 graphs=($(ls graph*.txt | sed 's/.txt//'))
 
 for i in "${graphs[@]}"
 do
-    echo "Running with graph file $i with $1 cores"
+    echo "Running with graph file $i with $ncores cores"
     ant -Dgraph=$i -Dncores=$ncores a2
 done

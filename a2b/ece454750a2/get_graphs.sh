c4f53f3d6e535590abf958c4d52eed061cace257
Fasih Awan <fasihawan@gmail.com>
2015-06-29 16:47:57 -0400

    Fixed get_graphs script

--- a2b/ece454750a2/get_graphs.sh
+++ a2b/ece454750a2/get_graphs.sh
@@ -7,17 +7,27 @@ then
 else
     echo "graph100K_A.txt not found, downloading"
     rm graph*.txt
-    wget http://ece.uwaterloo.ca/~wgolab/graphs.tar.gz
+    wget --no-check-certificate http://ece.uwaterloo.ca/~wgolab/graphs.tar.gz
     tar -xzvf graphs.tar.gz
     rm graphs.tar.gz
 fi
 
 rm graph*.sort
-wget https://ece.uwaterloo.ca/~wgolab/graph1K_B.sort
-wget https://ece.uwaterloo.ca/~wgolab/graph1K_C.sort
-wget https://ece.uwaterloo.ca/~wgolab/graph10K_B.sort
-wget https://ece.uwaterloo.ca/~wgolab/graph10K_C.sort
-wget https://ece.uwaterloo.ca/~wgolab/graph100K_A.sort
-wget https://ece.uwaterloo.ca/~wgolab/graph100K_B.sort
-wget https://ece.uwaterloo.ca/~wgolab/graph100K_C.sort
-wget https://ece.uwaterloo.ca/~wgolab/graph1M_B.sort
+sorts=($(ls graph*.txt | sed 's/.txt/.sort/'))
+
+echo "Getting sorted files..."
+
+for i in "${sorts[@]}"
+do
+    2>/dev/null 1>&2 wget --no-check-certificate https://ece.uwaterloo.ca/~wgolab/$i &
+done
+pid=$!
+spin='-\|/'
+
+i=0
+while kill -0 $pid 2>/dev/null
+do
+    i=$(( (i+1) %4 ))
+    printf "\r${spin:$i:1}"
+    sleep .1
+done

d62c5cc9c03c46da1fe1aa44eb32e1a86a0ffeca
Victor Lai <lai.victor.vl@gmail.com>
2015-07-03 03:41:05 -0400

    Code clean-up and comments

--- a2b/ece454750a2/src/ece454750s15a2/TriangleCountImpl.java
+++ a2b/ece454750a2/src/ece454750s15a2/TriangleCountImpl.java
@@ -64,70 +64,159 @@ public class TriangleCountImpl {
     }
 
     public List<Triangle> enumerateTriangles() throws IOException, InterruptedException, ExecutionException {
-        List<Triangle> triangles = enumerateTrianglesThreaded();
-        System.out.println("Number of triangles found: " + triangles.size());
-        return triangles;
-    }
 
-    private List<Triangle> enumerateTrianglesThreaded() throws IOException, InterruptedException, ExecutionException {
+        final Graph graph = new Graph(input);
 
-        final List<Triangle> triangles = new ArrayList<Triangle>();
-
-        Graph graph = new Graph(input);
+        List<Triangle> triangles;
 
         try {
-            long beginTime = System.nanoTime();
-
             graph.readParameters();
 
-            List<Integer>[] biggerNeighbours = new ArrayList[graph.numVertices];
-
-            for (int i = 0; i < graph.numVertices; i++) {
-                biggerNeighbours[i] = new ArrayList<Integer>();
+            if (numCores == 1) {
+                triangles = enumerateTrianglesSingleThreaded(graph);
+            } else {
+                triangles = enumerateTrianglesMultiThreaded(graph);
             }
-
-            final ExecutorService runnableExecutorService = Executors.newFixedThreadPool(numCores);
-
-            String line = null;
-            while ((line = graph.bufferedReader.readLine()) != null && !line.equals("")) {
-                runnableExecutorService.execute(new ParseInputEdgesRunnable(line, biggerNeighbours));
-            }
-
-            runnableExecutorService.shutdown();
-            runnableExecutorService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
-
-            long parseTime = System.nanoTime();
-            System.out.println("Parse time     : " + (parseTime - beginTime));
-
-            final List<Callable<List<Triangle>>> callables = new ArrayList<Callable<List<Triangle>>>();
-            for (int i = 0; i < numCores; i++) {
-                callables.add(new EnumerateTriangleCallable(i, numCores, biggerNeighbours));
-            }
-
-            final ExecutorService callableExecutorService = Executors.newFixedThreadPool(numCores);
-            final List<Future<List<Triangle>>> futures = new ArrayList<Future<List<Triangle>>>();
-            for (int i = 0; i < numCores; i++) {
-                futures.add(callableExecutorService.submit(callables.get(i)));
-            }
-
-            for (Future<List<Triangle>> future : futures) {
-                triangles.addAll(future.get());
-            }
-
-            long finishTime = System.nanoTime();
-            System.out.println("Triangle time  : " + (finishTime - parseTime));
-            System.out.println("Total time     : " + (finishTime - beginTime));
-
         } finally {
             graph.close();
         }
 
+        if (triangles == null) {
+            triangles = new ArrayList<Triangle>();
+        }
+
+        System.out.println("Number of triangles found: " + triangles.size());
+        return triangles;
+    }
+
+    private List<Triangle> enumerateTrianglesSingleThreaded(final Graph graph) throws IOException {
+
+        // initialize biggerNeighbours adjacency list
+        final ArrayList<Integer>[] biggerNeighbours = new ArrayList[graph.numVertices];
+        for (int i = 0; i < graph.numVertices; i++) {
+            biggerNeighbours[i] = new ArrayList<Integer>();
+        }
+
+        // parse graph into biggerNeighbours adjacency list
+        String line = null;
+        while ((line = graph.bufferedReader.readLine()) != null && !line.equals("")) {
+            parseBiggerNeighbourLine(biggerNeighbours, line);
+        }
+
+        // count triangles
+        final List<Triangle> triangles = enumerateTriangles(biggerNeighbours);
+
+        return triangles;
+    }
+
+    private List<Triangle> enumerateTrianglesMultiThreaded(final Graph graph) throws IOException, InterruptedException, ExecutionException {
+
+        // initialize biggerNeighbours adjacency list
+        final List<Integer>[] biggerNeighbours = new ArrayList[graph.numVertices];
+        for (int i = 0; i < graph.numVertices; i++) {
+            biggerNeighbours[i] = new ArrayList<Integer>();
+        }
+
+        // parse graph into biggerNeighbours adjacency list
+        final ExecutorService runnableExecutorService = Executors.newFixedThreadPool(numCores);
+
+        String line = null;
+        while ((line = graph.bufferedReader.readLine()) != null && !line.equals("")) {
+            runnableExecutorService.execute(new ParseInputEdgesRunnable(line, biggerNeighbours));
+        }
+
+        runnableExecutorService.shutdown();
+        runnableExecutorService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+
+        // count triangles
+        final List<Callable<List<Triangle>>> callables = new ArrayList<Callable<List<Triangle>>>();
+        for (int i = 0; i < numCores; i++) {
+            callables.add(new EnumerateTriangleCallable(i, numCores, biggerNeighbours));
+        }
+
+        final ExecutorService callableExecutorService = Executors.newFixedThreadPool(numCores);
+        final List<Future<List<Triangle>>> futures = new ArrayList<Future<List<Triangle>>>();
+        for (int i = 0; i < numCores; i++) {
+            futures.add(callableExecutorService.submit(callables.get(i)));
+        }
+
+        final List<Triangle> triangles = new ArrayList<Triangle>();
+        for (Future<List<Triangle>> future : futures) {
+            triangles.addAll(future.get());
+        }
+
+        return triangles;
+    }
+
+    private static void parseBiggerNeighbourLine(final List<Integer>[] biggerNeighbours, String line) {
+        final String parts[] = line.split(": ", 2);
+
+        final Integer vertex = Integer.parseInt(parts[0]);
+
+        final ArrayList<Integer> bigNeighbours = new ArrayList<Integer>();
+        if (parts.length > 1) {
+            final StringTokenizer tokenizer = new StringTokenizer(parts[1]);
+            while (tokenizer.hasMoreTokens()) {
+                final Integer edge = Integer.parseInt(tokenizer.nextToken());
+                if (edge > vertex) {
+                    bigNeighbours.add(edge);
+                }
+            }
+        }
+
+        Collections.sort(bigNeighbours);
+        biggerNeighbours[vertex] = new ArrayList<Integer>(bigNeighbours);
+    }
+
+    private static List<Triangle> enumerateTriangles(final List<Integer>[] biggerNeighbours) {
+        return enumerateTriangles(biggerNeighbours, 0, 1);
+    }
+
+    private static List<Triangle> enumerateTriangles(final List<Integer>[] biggerNeighbours, int initialPosition, int step) {
+        final List<Triangle> triangles = new ArrayList<Triangle>();
+
+        for (int smallVertex = initialPosition; smallVertex < biggerNeighbours.length; smallVertex += step) {
+
+            List<Integer> smallNeighbours = biggerNeighbours[smallVertex];
+
+            for (int mediumVertex : smallNeighbours) {
+
+                // from sorted ordering and pre-processing, all elements in smallNeighbours are larger than smallVertex
+
+                List<Integer> mediumNeighbours = biggerNeighbours[mediumVertex];
+
+                // small and medium are sorted min->max
+                // iterate two pointers backwards from max->min searching for equal elements
+
+                Integer i_s = smallNeighbours.size()-1;
+                Integer i_m = mediumNeighbours.size()-1;
+
+                while (i_s >= 0 && i_m >= 0) {
+
+                    final int check_s = smallNeighbours.get(i_s);
+                    final int check_m = mediumNeighbours.get(i_m);
+
+                    if (check_m <= smallVertex || check_s <= mediumVertex) {
+                        break;
+                    }
+
+                    if (check_s < check_m) {
+                        --i_m;
+                    } else if (check_s > check_m) {
+                        --i_s;
+                    } else {
+                        triangles.add(new Triangle(smallVertex, mediumVertex, check_m));
+                        --i_s;
+                        --i_m;
+                    }
+                }
+            }
+        }
 
         return triangles;
     }
 
     private class ParseInputEdgesRunnable implements Runnable {
-
         final String line;
         final List<Integer>[] biggerNeighbours;
 
@@ -138,32 +227,13 @@ public class TriangleCountImpl {
             this.biggerNeighbours = biggerNeighbours;
         }
 
-
         @Override
         public void run() {
-            String parts[] = line.split(": ", 2);
-
-            final Integer vertex = Integer.parseInt(parts[0]);
-
-            final ArrayList<Integer> bigNeighbours = new ArrayList<Integer>();
-
-            if (parts.length > 1) {
-                StringTokenizer tokenizer = new StringTokenizer(parts[1]);
-                while (tokenizer.hasMoreTokens()) {
-                    final Integer edge = Integer.parseInt(tokenizer.nextToken());
-                    if (edge > vertex) {
-                        bigNeighbours.add(edge);
-                    }
-                }
-            }
-
-            Collections.sort(bigNeighbours);
-            biggerNeighbours[vertex] = new ArrayList<Integer>(bigNeighbours);
+            parseBiggerNeighbourLine(biggerNeighbours, line);
         }
     }
 
     private class EnumerateTriangleCallable implements Callable<List<Triangle>> {
-
         private final int initialPosition;
         private final int step;
         private final List<Integer>[] biggerNeighbours;
@@ -179,210 +249,7 @@ public class TriangleCountImpl {
 
         @Override
         public List<Triangle> call() {
-            final List<Triangle> triangles = new ArrayList<Triangle>();
-
-            for (int smallVertex = initialPosition; smallVertex < biggerNeighbours.length; smallVertex += step) {
-
-                List<Integer> smallNeighbours = biggerNeighbours[smallVertex];
-
-                if (smallNeighbours == null) {
-                    continue;
-                }
-
-                for (int mediumVertex : smallNeighbours) {
-
-                     List<Integer> mediumNeighbours = biggerNeighbours[mediumVertex];
-
-                    if (mediumNeighbours == null) {
-                        continue;
-                    }
-
-                    Integer i_s = smallNeighbours.size()-1;
-                    Integer i_m = mediumNeighbours.size()-1;
-
-                    while (i_s >= 0 && i_m >= 0) {
-
-                        final int check_s = smallNeighbours.get(i_s);
-                        final int check_m = mediumNeighbours.get(i_m);
-
-                        if (check_m <= smallVertex || check_s <= mediumVertex) {
-                            break;
-                        }
-
-                        if (check_s < check_m) {
-                            --i_m;
-                        } else if (check_s > check_m) {
-                            --i_s;
-                        } else {
-                            triangles.add(new Triangle(smallVertex, mediumVertex, check_m));
-                            --i_s;
-                            --i_m;
-                        }
-                    }
-                }
-            }
-
-            return triangles;
+            return enumerateTriangles(biggerNeighbours, initialPosition, step);
         }
     }
-
-    public List<Triangle> enumerateTrianglesTest() throws IOException, InterruptedException
-    {
-        List<Integer>[] adjacencyLists = getAdjacencyLists();
-        ExecutorService executor = Executors.newFixedThreadPool(numCores);
-        List<Triangle> triangles = Collections.synchronizedList(new ArrayList<Triangle>());
-
-        for(int i = 0; i < numCores; i++)
-        {
-            Worker worker = new Worker(adjacencyLists, i, triangles);
-            executor.execute(worker);
-        }
-
-        executor.shutdown();
-        executor.awaitTermination(2L, TimeUnit.MINUTES);
-
-        return triangles;
-    }
-
-    private class Worker implements Runnable
-    {
-        private final List<Integer>[] _adjacencyLists;
-        private final int _startingIndex;
-        private final List<Triangle> _triangles;
-
-        Worker(List<Integer>[] adjacencyLists,int startingIndex, List<Triangle> triangles)
-        {
-            _adjacencyLists = adjacencyLists;
-            _startingIndex = startingIndex;
-            _triangles = triangles;
-        }
-
-        @Override
-        public void run()
-        {
-            List<Triangle> localTriangles = new ArrayList<Triangle>();
-
-            for(int firstVertex = _startingIndex; firstVertex < _adjacencyLists.length; firstVertex += numCores)
-            {
-                List<Integer> smallerEdges = _adjacencyLists[firstVertex];
-
-                if(null == smallerEdges)
-                {
-                    continue;
-                }
-
-                for(int secondVertex : smallerEdges)
-                {
-                    List<Integer> mediumEdges = _adjacencyLists[secondVertex];
-
-                    if(null == mediumEdges)
-                    {
-                        continue;
-                    }
-
-                    int i_s = smallerEdges.size() - 1;
-                    int i_m = mediumEdges.size() - 1;
-                    while(i_s >= 0 && i_m >= 0)
-                    {
-                        int check_s = smallerEdges.get(i_s);
-                        int check_m = mediumEdges.get(i_m);
-                        if(check_s == check_m)
-                        {
-                            localTriangles.add(new Triangle(firstVertex, secondVertex, check_s));
-                            --i_m;
-                            --i_s;
-                        }
-                        else if(check_s > check_m)
-                        {
-                            --i_s;
-                        }
-                        else
-                        {
-                            if(check_s < secondVertex)
-                            {
-                                break;
-                            }
-                            --i_m;
-                        }
-                    }
-                }
-            }
-
-            _triangles.addAll(localTriangles);
-        }
-    }
-
-    private List<Integer>[] getAdjacencyLists() throws IOException
-    {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(input)));
-
-        try
-        {
-            String headerLine = reader.readLine();
-
-            if (!headerLine.contains("vertices") || !headerLine.contains("edges"))
-            {
-                System.err.println("Invalid graph file format. Offending line: " + headerLine);
-                System.exit(-1);
-            }
-
-            String parts[] = headerLine.split(", ");
-            int numVertices = Integer.parseInt(parts[0].split(" ")[0]);
-            int numEdges = Integer.parseInt(parts[1].split(" ")[0]);
-            System.out.println("Found graph with " + numVertices + " vertices and " + numEdges + " edges");
-
-            return getAdjacencyLists(reader, numVertices);
-        }
-        finally
-        {
-            reader.close();
-        }
-    }
-
-    private List<Integer>[] getAdjacencyLists(BufferedReader reader, int numVertices) throws IOException
-    {
-        List<Integer>[] adjacencyLists = new List[numVertices];
-        String line;
-
-        while(null != (line = reader.readLine()) && !line.isEmpty())
-        {
-            String[] vertexDescriptor = line.split(": ");
-
-            // If the vertex has no adjacent vertices
-            if(vertexDescriptor.length <= 1)
-            {
-                continue;
-            }
-
-            int vertexID = Integer.parseInt(vertexDescriptor[0]);
-            String[] adjacentVertices = vertexDescriptor[1].split(" ");
-
-            // If the vertex has less than 2 adjacent vertices, then it can't be a part of a triangle
-            if(adjacentVertices.length <= 1)
-            {
-                continue;
-            }
-
-            List<Integer> adjacentVerticesSet = new ArrayList<Integer>(adjacentVertices.length);
-
-            for(String adjacentVertex : adjacentVertices)
-            {
-                int adjacentVertexID = Integer.parseInt(adjacentVertex);
-
-                if(adjacentVertexID > vertexID)
-                {
-                    adjacentVerticesSet.add(adjacentVertexID);
-                }
-            }
-
-            if(!adjacentVerticesSet.isEmpty())
-            {
-                Collections.sort(adjacentVerticesSet);
-                adjacencyLists[vertexID] = adjacentVerticesSet;
-            }
-        }
-
-        return adjacencyLists;
-    }
-
 }

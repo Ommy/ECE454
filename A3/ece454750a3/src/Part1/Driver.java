4e398a8136866f69b2bbb7ca72b03549fc2926ee
Fasih Awan <fasihawan@gmail.com>
2015-07-16 23:25:48 -0400

    Finished Driver based on tutorial

--- A3/ece454750a3/src/Part1/Driver.java
+++ A3/ece454750a3/src/Part1/Driver.java
@@ -1,18 +1,31 @@
 package Part1;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 import java.io.IOException;
 
 public class Driver {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
         Configuration conf = new Configuration();
         Job job = Job.getInstance(conf, "Part 1");
+
         job.setJarByClass(Driver.class);
         job.setMapperClass(GeneMapper.class);
+        job.setReducerClass(GeneReducer.class);
+        job.setCombinerClass(GeneReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(DoubleWritable.class);
 
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
 
     }
 }

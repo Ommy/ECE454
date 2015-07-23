1c21068c69a36a8f79c2c0340617e1d2d77aeb63
Victor Lai <lai.victor.vl@gmail.com>
2015-07-22 22:42:19 -0400

    Partial Part 3 cleanup

--- A3/ece454750a3/part3/src/Part3.java
+++ A3/ece454750a3/part3/src/Part3.java
@@ -1,6 +1,6 @@
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.*;
-import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -10,49 +10,41 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 import java.io.IOException;
 import java.math.BigDecimal;
-import java.math.MathContext;
-import java.text.DecimalFormat;
-import java.text.ParseException;
 
 public class Part3 {
 
-    enum GeneEnum { SampleCounter }
+    enum GeneEnum {NumberOfSamplesCounter}
+
     private static final String TEMP_OUTPUT_PATH = "temp_path";
+    private static final String NUMBER_OF_SAMPLES_COUNTER = "number_of_samples_counter";
 
-    public static class GeneDotProductReducer extends Reducer<Text, Text, Text, Text> {
-
-        private final Text mValue = new Text();
+    public static class GeneDotProductReducer extends Reducer<Text, Text, NullWritable, Text> {
 
         @Override
         public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
             String[] left = values.iterator().next().toString().split(",");
             String[] right = values.iterator().next().toString().split(",");
 
-            DecimalFormat fmt = new DecimalFormat();
-            fmt.setParseBigDecimal(true);
-
             BigDecimal sum = BigDecimal.ZERO;
-            for (Integer i = 0; i < left.length; i++) {
-                left[i] = left[i].trim();
-                right[i] = right[i].trim();
-                if (left[i].equals("0.0") || right[i].equals("0.0")) {
+            for (Integer expression = 0; expression < left.length; expression++) {
+                left[expression] = left[expression].trim();
+                right[expression] = right[expression].trim();
+
+                if (left[expression].equals("0.0") || right[expression].equals("0.0")) {
                     continue;
                 }
 
-
                 try {
-                    BigDecimal a = new BigDecimal(left[i]);
-                    BigDecimal b = new BigDecimal(right[i]);
+                    BigDecimal a = new BigDecimal(left[expression]);
+                    BigDecimal b = new BigDecimal(right[expression]);
                     sum = sum.add(a.multiply(b));
                 } catch (NumberFormatException e) {
-                    System.out.println("wtf: " + left[i] + ", " + right[i]);
                     e.printStackTrace();
                 }
             }
 
             if (sum.compareTo(BigDecimal.ZERO) != 0) {
-                mValue.set(sum.toPlainString());
-                context.write(key, mValue);
+                context.write(NullWritable.get(), new Text(key.toString() + "," + sum.toPlainString()));
             }
         }
     }
@@ -61,42 +53,34 @@ public class Part3 {
 
         private final Text mKey = new Text();
         private final Text mValue = new Text();
-        private long mapperCounter;
+        private Long numberOfSamples;
 
         @Override
         public void map(Object inKey, Text value, Context context) throws IOException, InterruptedException {
-            mapperCounter = Long.parseLong(context.getConfiguration().get("counter"));
+            numberOfSamples = Long.parseLong(context.getConfiguration().get(NUMBER_OF_SAMPLES_COUNTER));
             String[] input = value.toString().split(",", 2);
-            int key = 0;
-            for (int i = 1; i <= mapperCounter; i++) {
-                key = Integer.parseInt(input[0]);
-                if (key < i) {
-                    mKey.set("sample_" + key + "," + "sample_" + i);
-                    mValue.set(input[1]);
-                    context.write(mKey, mValue);
-                } else if (key > i) {
-                    mKey.set("sample_" + i + "," + "sample_" + key);
-                    mValue.set(input[1]);
-                    context.write(mKey, mValue);
+
+            Integer currentSample = Integer.parseInt(input[0].replace("sample_", ""));
+            mValue.set(input[1]);
+
+            for (int sample = 1; sample <= numberOfSamples; sample++) {
+                if (currentSample < sample) {
+                    mKey.set("sample_" + currentSample + "," + "sample_" + sample);
+                } else if (currentSample > sample) {
+                    mKey.set("sample_" + sample + "," + "sample_" + currentSample);
                 }
+
+                context.write(mKey, mValue);
             }
         }
     }
 
-    public static class GeneMapper extends Mapper<Object, Text, Text, Text> {
-
-        private final Text mKey = new Text();
-        private final Text mValue = new Text();
+    public static class GeneMapper extends Mapper<Object, Text, NullWritable, Text> {
 
         @Override
         public void map(Object key, Text text, Context context) throws IOException, InterruptedException {
-            String[] values = text.toString().split(",", 2);
-
-            mKey.set(values[0].replace("sample_", "") + ",");
-            mValue.set(values[1]);
-
-            context.write(mKey, mValue);
-            context.getCounter(GeneEnum.SampleCounter).increment(1L);
+            context.write(NullWritable.get(), text);
+            context.getCounter(GeneEnum.NumberOfSamplesCounter).increment(1L);
         }
 
     }
@@ -108,7 +92,7 @@ public class Part3 {
         job1.setJarByClass(Part3.class);
         job1.setMapperClass(Part3.GeneMapper.class);
 
-        job1.setMapOutputKeyClass(Text.class);
+        job1.setMapOutputKeyClass(NullWritable.class);
         job1.setMapOutputValueClass(Text.class);
 
         job1.setInputFormatClass(TextInputFormat.class);
@@ -116,11 +100,12 @@ public class Part3 {
 
         TextInputFormat.addInputPath(job1, new Path(args[0]));
         TextOutputFormat.setOutputPath(job1, new Path(TEMP_OUTPUT_PATH));
+
         job1.waitForCompletion(true);
 
+        Long counter = job1.getCounters().findCounter(GeneEnum.NumberOfSamplesCounter).getValue();
+        conf.set(NUMBER_OF_SAMPLES_COUNTER, counter.toString());
 
-        String counter = job1.getCounters().findCounter(GeneEnum.SampleCounter).getValue() + "";
-        conf.set("counter", job1.getCounters().findCounter(GeneEnum.SampleCounter).getValue() + "");
         Job job2 = Job.getInstance(conf,"Job 2");
         System.out.println("Counter in Main: " + counter);
 
@@ -131,7 +116,7 @@ public class Part3 {
         job2.setMapOutputKeyClass(Text.class);
         job2.setMapOutputValueClass(Text.class);
 
-        job2.setOutputKeyClass(Text.class);
+        job2.setOutputKeyClass(NullWritable.class);
         job2.setOutputValueClass(Text.class);
 
         job2.setInputFormatClass(TextInputFormat.class);
@@ -139,6 +124,7 @@ public class Part3 {
 
         TextInputFormat.addInputPath(job2, new Path(TEMP_OUTPUT_PATH));
         TextOutputFormat.setOutputPath(job2, new Path(args[1]));
+
         job2.waitForCompletion(true);
 
         FileSystem fs = FileSystem.get(conf);
